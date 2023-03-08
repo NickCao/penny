db5cd2165777515e249f0e135eafca2c88c80305
Nick Cao <nickcao@nichi.co>
2023-03-08 17:25:11 +0800

    reduce unsafe in example.rs

--- nccl-net-homa/src/bin/example.rs
+++ nccl-net-homa/src/bin/example.rs
@@ -27,79 +27,77 @@ unsafe extern "C" fn logger(
 }
 
 fn main() {
-    unsafe {
-        let ret = init(Some(logger));
+    let ret = init(Some(logger));
+    assert_eq!(ret, ncclResult_t::ncclSuccess);
+    let mut ndev = 0;
+    let ret = devices(&mut ndev);
+    assert_eq!(ret, ncclResult_t::ncclSuccess);
+    for dev in 0..ndev {
+        let mut props = unsafe { std::mem::zeroed() };
+        let ret = get_properties(dev, &mut props);
         assert_eq!(ret, ncclResult_t::ncclSuccess);
-        let mut ndev = 0;
-        let ret = devices(&mut ndev);
-        assert_eq!(ret, ncclResult_t::ncclSuccess);
-        for dev in 0..ndev {
-            let mut props = std::mem::zeroed();
-            let ret = get_properties(dev, &mut props);
-            assert_eq!(ret, ncclResult_t::ncclSuccess);
-        }
-        let mut handle = [0u8; NCCL_NET_HANDLE_MAXSIZE as usize];
-        let mut listen_comm: *mut c_void = null_mut();
-        let ret = listen(0, handle.as_mut_ptr().cast(), &mut listen_comm);
-        assert_eq!(ret, ncclResult_t::ncclSuccess);
-
-        let mut send_comm: *mut c_void = null_mut();
-        let ret = connect(0, handle.as_mut_ptr().cast(), &mut send_comm);
-        assert_eq!(ret, ncclResult_t::ncclSuccess);
-
-        let mut recv_comm: *mut c_void = null_mut();
-        let ret = accept(listen_comm, &mut recv_comm);
-        assert_eq!(ret, ncclResult_t::ncclSuccess);
-
-        let mut data = b"hello penny\0".to_vec();
-        let mut send_req: *mut c_void = null_mut();
-        let ret = isend(
-            send_comm,
-            data.as_mut_ptr().cast(),
-            data.len().try_into().unwrap(),
-            0,
-            null_mut(),
-            &mut send_req,
-        );
-        assert_eq!(ret, ncclResult_t::ncclSuccess);
-
-        let mut recv_req: *mut c_void = null_mut();
-        let mut buf = vec![0u8; 100];
-        let mut bufs = [buf.as_mut_ptr().cast()];
-        let mut sizes = [data.len().try_into().unwrap()];
-        let ret = irecv(
-            recv_comm,
-            1,
-            bufs.as_mut_ptr(),
-            sizes.as_mut_ptr(),
-            null_mut(),
-            null_mut(),
-            &mut recv_req,
-        );
-        assert_eq!(ret, ncclResult_t::ncclSuccess);
-
-        loop {
-            let mut done = 0;
-            let mut size = 0;
-            let ret = test(recv_req, &mut done, &mut size);
-            assert_eq!(ret, ncclResult_t::ncclSuccess);
-            if done == 1 {
-                assert_eq!(size, data.len().try_into().unwrap());
-                break;
-            }
-        }
-
-        loop {
-            let mut done = 0;
-            let mut size = 0;
-            let ret = test(send_req, &mut done, &mut size);
-            assert_eq!(ret, ncclResult_t::ncclSuccess);
-            if done == 1 {
-                assert_eq!(size, data.len().try_into().unwrap());
-                break;
-            }
-        }
-
-        assert_eq!(buf[..data.len()], data);
     }
+    let mut handle = [0u8; NCCL_NET_HANDLE_MAXSIZE as usize];
+    let mut listen_comm: *mut c_void = null_mut();
+    let ret = unsafe { listen(0, handle.as_mut_ptr().cast(), &mut listen_comm) };
+    assert_eq!(ret, ncclResult_t::ncclSuccess);
+
+    let mut send_comm: *mut c_void = null_mut();
+    let ret = unsafe { connect(0, handle.as_mut_ptr().cast(), &mut send_comm) };
+    assert_eq!(ret, ncclResult_t::ncclSuccess);
+
+    let mut recv_comm: *mut c_void = null_mut();
+    let ret = unsafe { accept(listen_comm, &mut recv_comm) };
+    assert_eq!(ret, ncclResult_t::ncclSuccess);
+
+    let mut data = b"hello penny\0".to_vec();
+    let mut send_req: *mut c_void = null_mut();
+    let ret = isend(
+        send_comm,
+        data.as_mut_ptr().cast(),
+        data.len().try_into().unwrap(),
+        0,
+        null_mut(),
+        &mut send_req,
+    );
+    assert_eq!(ret, ncclResult_t::ncclSuccess);
+
+    let mut recv_req: *mut c_void = null_mut();
+    let mut buf = vec![0u8; 100];
+    let mut bufs = [buf.as_mut_ptr().cast()];
+    let mut sizes = [data.len().try_into().unwrap()];
+    let ret = irecv(
+        recv_comm,
+        1,
+        bufs.as_mut_ptr(),
+        sizes.as_mut_ptr(),
+        null_mut(),
+        null_mut(),
+        &mut recv_req,
+    );
+    assert_eq!(ret, ncclResult_t::ncclSuccess);
+
+    loop {
+        let mut done = 0;
+        let mut size = 0;
+        let ret = test(recv_req, &mut done, &mut size);
+        assert_eq!(ret, ncclResult_t::ncclSuccess);
+        if done == 1 {
+            assert_eq!(size, data.len().try_into().unwrap());
+            break;
+        }
+    }
+
+    loop {
+        let mut done = 0;
+        let mut size = 0;
+        let ret = test(send_req, &mut done, &mut size);
+        assert_eq!(ret, ncclResult_t::ncclSuccess);
+        if done == 1 {
+            assert_eq!(size, data.len().try_into().unwrap());
+            break;
+        }
+    }
+
+    assert_eq!(buf[..data.len()], data);
 }
